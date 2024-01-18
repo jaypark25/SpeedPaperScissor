0f477f51a0cbdb753f6e96c13aed4a2fb2c825a9
Jay Park <jaybinpark@gmail.com>
2024-01-18 07:14:42 -0800

    Initial Commit

---
+++ SpeedPaperScissor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedPaperScissor
+//
+//  Created by Jay Park on 2024-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedPaperScissor/SpeedPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedPaperScissorApp.swift
+//  SpeedPaperScissor
+//
+//  Created by Jay Park on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct SpeedPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
